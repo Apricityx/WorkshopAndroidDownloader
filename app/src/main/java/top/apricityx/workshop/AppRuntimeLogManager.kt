c10eeee8a1494d98785a6fd82e387153ed94a3de
apricityx <apricityx@qq.com>
2026-04-13 00:57:32 +0800

    chore(release): prepare v1.1.6

--- app/src/main/java/top/apricityx/workshop/AppRuntimeLogManager.kt
+++ app/src/main/java/top/apricityx/workshop/AppRuntimeLogManager.kt
@@ -5,6 +5,7 @@ import android.content.ContentValues
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
+import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
 import com.elvishew.xlog.LogConfiguration
 import com.elvishew.xlog.XLog
@@ -263,6 +264,7 @@ object AppRuntimeLogManager {
             exportBundleToLegacyDownloads(application, bundleFile)
         }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     private fun exportBundleToMediaStore(
         application: Application,
         bundleFile: File,
